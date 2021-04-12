0f8b3a1f3c0c227c3cfbcdf41e2d731a8d314538
Anton_Zaparivanniy <zaparivannyi@gmail.com>
2021-04-12 03:56:13 +0300

    Init project

---
+++ Cormen.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Cormen.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
