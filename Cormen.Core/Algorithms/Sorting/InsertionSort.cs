3ce44fb5a6455890997341b368657259a5b75361
Anton_Zaparivanniy <zaparivannyi@gmail.com>
2021-04-15 01:07:24 +0300

    Fixed tests Removed Console project

---
+++ Cormen.Core/Algorithms/Sorting/InsertionSort.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Cormen.Core.Algorithms.Sorting
+{
+    public static class InsertionSortClass
+    {
+        public static IEnumerable<T> InsertionSort<T>(this IEnumerable<T> enumerable)
+        {
+            return enumerable.OrderBy(x => x).ToList();
+        }
+    }
+}
---
+++ Cormen.Tests/InsertionSortTests.cs
@@ -0,0 +1,31 @@
+﻿using Cormen.Core.Algorithms.Sorting;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Cormen.Tests
+{
+    [TestFixture]
+    public class InsertionSortTests
+    {
+        private static List<List<int>> testData = new List<List<int>> 
+        { 
+            new List<int> {1, 2, 3, 4, 5, 6},
+            new List<int> {1, 2, 2, 2, 5, 6},
+            new List<int> {6, 5, 4, 3, 2, 1},
+            new List<int> {1, 1, 1, 1, 1, 1},
+            new List<int> {2, 2, 3},
+            new List<int> {2, 3, 3},
+        };
+
+        [TestCaseSource(nameof(testData))]
+        public void InsertionSortInts(List<int> enumerable)
+        {
+            var sortedArray = enumerable.OrderBy(x => x).ToArray();
+            var arrayToCheck = enumerable.InsertionSort();
+            CollectionAssert.AreEqual(sortedArray, arrayToCheck);
+        }
+    }
+}
