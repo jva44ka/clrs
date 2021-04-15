2c77cce907cdc6ccb530833c64782b23c851c8d4
Anton_Zaparivanniy <zaparivannyi@gmail.com>
2021-04-15 11:35:40 +0300

    Insertion sort implementation

--- Cormen.Core/Algorithms/Sorting/InsertionSort.cs
+++ Cormen.Core/Algorithms/Sorting/InsertionSort.cs
@@ -3,10 +3,26 @@ using System.Linq;
 
 namespace Cormen.Core.Algorithms.Sorting
 {
+    // Сортировка вставками
     public class InsertionSortClass
     {
-        public static IEnumerable<T> Sort<T>(IEnumerable<T> enumerable)
+        public static IEnumerable<int> Sort(IList<int> enumerable)
         {
+            int key;
+            int i;
+
+            for (int j = 2; j < enumerable.Count(); j++)
+            {
+                key = enumerable[j];
+                i = j - 1;
+
+                while (i > 0 && enumerable[i] > key)
+                {
+                    enumerable[i + 1] = enumerable[i];
+                    i--;
+                }
+                enumerable[i + 1] = key;
+            }
             return enumerable.OrderBy(x => x).ToList();
         }
     }
