0bba3189dacd09f5355b26e07b5bb5995df7fe31
Anton_Zaparivanniy <zaparivannyi@gmail.com>
2021-04-18 19:12:07 +0300

    Merge sort

---
+++ Cormen.Core/Algorithms/Sorting/Interfaces/ISort.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Cormen.Core.Algorithms.Sorting.Interfaces
+{
+    public interface ISort
+    {
+        // По неубыванию (возрастанию)
+        public IEnumerable<int> Sort(IList<int> enumerable);
+
+        // По невозрастанию (убыванию)
+        public IEnumerable<int> SortDesc(IList<int> enumerable);
+    }
+}
