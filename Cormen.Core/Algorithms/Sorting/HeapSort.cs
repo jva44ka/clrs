3238ca8269a7d034e72518eb164aa65750087660
Anton_Zaparivanniy <zaparivannyi@gmail.com>
2021-04-21 01:15:36 +0300

    Added heapsort (1 test case failed)

---
+++ Cormen.Core/Algorithms/Sorting/HeapSort.cs
@@ -0,0 +1,88 @@
+﻿using Cormen.Core.Algorithms.Sorting.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cormen.Core.Algorithms.Sorting
+{
+    public class HeapSort : ISort
+    {
+        #region ascending
+        public IEnumerable<int> Sort(IList<int> enumerable)
+        {
+            return HeapSortFunc(enumerable, enumerable.Count);
+        }
+
+        int Parent(int i)
+        {
+            return i / 2;
+        }
+
+        int Left(int i)
+        {
+            return 2 * i;
+        }
+
+        int Right(int i)
+        {
+            return 2 * i + 1;
+        }
+
+        //Поддержание свойста невозрастающей пирамиды
+        void MaxHepify(IList<int> enumerable, int heapSize, int i)
+        {
+            int l = Left(i);
+            int r = Right(i);
+            int largest;
+            int buffer;
+
+            if (l <= (heapSize - 1) && enumerable[l] > enumerable[i])
+                largest = l;
+            else
+                largest = i;
+
+            if (r <= (heapSize - 1) && enumerable[r] > enumerable[largest])
+                largest = r;
+
+            if (largest != i)
+            {
+                SwapArrayElements(enumerable, i, largest);
+                MaxHepify(enumerable, heapSize, largest);
+            }
+        }
+
+        void BuildMaxHeap(IList<int> enumerable, int heapSize)
+        {
+            for (int i = heapSize / 2 - 1; i >= 0; i--)
+                MaxHepify(enumerable, heapSize, i);
+
+        }
+
+        IList<int> HeapSortFunc(IList<int> enumerable, int heapSize)
+        {
+            BuildMaxHeap(enumerable, heapSize);
+            for (int i = heapSize - 1; i > 0; i--)
+            {
+                SwapArrayElements(enumerable, 0, i);
+                MaxHepify(enumerable, --heapSize, 0);
+            }
+            return enumerable;
+        }
+
+        // TODO Сделать обмен элементами без буфера
+        private void SwapArrayElements(IList<int> list, int elIndex1, int elIndex2)
+        {
+            int buffer = list[elIndex1];
+            list[elIndex1] = list[elIndex2];
+            list[elIndex2] = buffer;
+        }
+        #endregion
+
+        #region descending
+        public IEnumerable<int> SortDesc(IList<int> enumerable)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+}
---
+++ Cormen.Tests/HeapSortTests.cs
@@ -0,0 +1,37 @@
+﻿using Cormen.Core.Algorithms.Sorting;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Cormen.Tests
+{
+    [TestFixture(Category = "Sorting")]
+    public class HeapSortTests
+    {
+        private static List<List<int>> testData = new List<List<int>>
+        {
+            new List<int> {1, 2, 3, 4, 5, 6},
+            new List<int> {1, 2, 2, 2, 5, 6},
+            new List<int> {6, 5, 4, 3, 2, 1},
+            new List<int> {1, 1, 1, 1, 1, 1},
+            new List<int> {2, 2, 3},
+            new List<int> {2, 3, 3},
+        };
+
+        [TestCaseSource(nameof(testData))]
+        public void HeapSortInts(List<int> enumerable)
+        {
+            var sortedArray = enumerable.OrderBy(x => x).ToArray();
+            var arrayToCheck = new HeapSort().Sort(enumerable);
+            CollectionAssert.AreEqual(sortedArray, arrayToCheck);
+        }
+
+        [TestCaseSource(nameof(testData))]
+        public void HeapSortInts_Desc(List<int> enumerable)
+        {
+            var sortedArray = enumerable.OrderByDescending(x => x).ToArray();
+            var arrayToCheck = new HeapSort().SortDesc(enumerable);
+            CollectionAssert.AreEqual(sortedArray, arrayToCheck);
+        }
+    }
+}
