3aa47724d7a615cfb72c811b5327a4e4c16db598
Anton_Zaparivanniy <zaparivannyi@gmail.com>
2021-04-25 03:06:04 +0300

    Added quicksort

---
+++ Cormen.Core/Algorithms/Sorting/QuickSort.cs
@@ -0,0 +1,53 @@
+﻿using Cormen.Core.Algorithms.Sorting.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cormen.Core.Algorithms.Sorting
+{
+    public class QuickSort : ISortable
+    {
+        public IList<int> Sort(IList<int> enumerable)
+        {
+            QuickSortAsc(enumerable, 0, enumerable.Count - 1);
+            return enumerable;
+        }
+
+        public IList<int> SortDesc(IList<int> enumerable)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void QuickSortAsc(IList<int> enumerable, int p, int r)
+        {
+            if (p < r)
+            {
+                var q = Partition(enumerable, p, r);
+                QuickSortAsc(enumerable, p, q - 1);
+                QuickSortAsc(enumerable, q + 1, r);
+            }
+        }
+
+        private int Partition(IList<int> enumerable, int p, int r)
+        {
+            var i = p - 1;
+            for (int j = p; j < r; j++)
+            {
+                if (enumerable[j] <= enumerable[r])
+                {
+                    i++;
+                    Swap(enumerable, i, j);
+                }
+            }
+            Swap(enumerable, i + 1, r);
+            return i + 1;
+        }
+
+        private void Swap(IList<int> enumerable, int elIndex1, int elIndex2)
+        {
+            int buffer = enumerable[elIndex1];
+            enumerable[elIndex1] = enumerable[elIndex2];
+            enumerable[elIndex2] = buffer;
+        } 
+    }
+}
---
+++ Cormen.Tests/QuickSortTests.cs
@@ -0,0 +1,27 @@
+﻿using Cormen.Core.Algorithms.Sorting;
+using Cormen.Tests.Data;
+using Cormen.Tests.Generic;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cormen.Tests
+{
+    public class QuickSortTests
+    {
+        private static List<List<int>> _testData = SortingData.TestData;
+
+        [TestCaseSource(nameof(_testData))]
+        public void QuickSortInts(List<int> enumerable)
+        {
+            GenericSortTests.Test(enumerable, new QuickSort());
+        }
+
+        [TestCaseSource(nameof(_testData))]
+        public void QuickSortInts_Desc(List<int> enumerable)
+        {
+            GenericSortTests.Test(enumerable, new QuickSort());
+        }
+    }
+}
