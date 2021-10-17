e5ab4d50fa773cda8a0da6224365d6287a78f9a3
Anton_Zaparivanniy <zaparivannyi@gmail.com>
2021-10-17 06:32:59 +0300

    Рефакторинг тестов (говорящие названия тестовых методов, структура тестов, комменты Arrange/Act/Assert) Изменение проверки дерева (не через обход дерева а через стаб, так намного правильнее, проверяется именно структура дерева)

--- CLRS.Tests/Tests/Structures/BSTTests.cs
+++ CLRS.Tests/Tests/Structures/BSTTests.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
 using CLRS.Core.Structures;
 using CLRS.Tests.Generators;
 using CLRS.Tests.Stubs;
@@ -15,88 +14,78 @@ namespace CLRS.Tests.Tests.Structures
         private readonly int _value3 = 3;
 
         [Test]
-        public void Add321()
+        public void Insert_321KeysAndValues_ShouldBe123Tree()
         {
             //Arrange
-            var actualTree = new BST<string, int>();
-            actualTree.Insert(_value3.ToString(), _value3);
-            actualTree.Insert(_value1.ToString(), _value1);
-            actualTree.Insert(_value2.ToString(), _value2);
-
             var expectedTree = new BinaryTreeNodeStub<string, int>(_value3.ToString(), _value3);
             expectedTree.WithLeftNode(_value1.ToString(), _value1)
                         .ToLeftNode()
                         .WithRightNode(_value2.ToString(), _value2);
+            var actualTree = new BST<string, int>();
+
+            //Act
+            actualTree.Insert(_value3.ToString(), _value3);
+            actualTree.Insert(_value1.ToString(), _value1);
+            actualTree.Insert(_value2.ToString(), _value2);
+            
+            //Assert
+            Assert.AreEqual(expectedTree, actualTree.Root);
+        }
+
+        [Test]
+        public void Insert_231KeysAndValues_ShouldBe123Tree()
+        {
+            //Arrange
+            var expectedTree = new BinaryTreeNodeStub<string, int>(_value2.ToString(), _value2);
+            expectedTree.WithRightNode(_value3.ToString(), _value3)
+                        .WithLeftNode(_value1.ToString(), _value1);
+            var actualTree = new BST<string, int>();
+
+            //Act
+            actualTree.Insert(_value2.ToString(), _value2);
+            actualTree.Insert(_value3.ToString(), _value3);
+            actualTree.Insert(_value1.ToString(), _value1);
 
             //Assert
             Assert.AreEqual(expectedTree, actualTree.Root);
         }
 
         [Test]
-        public void Add231()
+        public void Delete_Key2From123Tree_ShouldBe13Tree()
         {
-            var value1 = 1;
-            var value2 = 2;
-            var value3 = 3;
+            //Arrange
+            var expectedTree = new BinaryTreeNodeStub<string, int>(_value3.ToString(), _value3);
+            expectedTree.WithLeftNode(_value1.ToString(), _value1);
 
-            var tree = new BST<string, int>();
+            var actualTree = new BST<string, int>();
+            actualTree.Insert(_value3.ToString(), _value3);
+            actualTree.Insert(_value1.ToString(), _value1);
+            actualTree.Insert(_value2.ToString(), _value2);
 
-            tree.Insert(value2.ToString(), value2);
-            tree.Insert(value3.ToString(), value3);
-            tree.Insert(value1.ToString(), value1);
-            CollectionAssert.AreEqual(new List<int> { 1, 2, 3 }, tree.InOrderTreeWalk());
+            //Act
+            actualTree.Delete(_value2.ToString());
+
+            //Assert
+            Assert.AreEqual(expectedTree, actualTree.Root);
         }
 
         [Test]
-        public void AddAndRemove()
+        public void Reverse_WithTree123_ShouldBeTree321()
         {
-            var value1 = 1;
-            var value2 = 2;
-            var value3 = 3;
+            //Arrange
+            var expectedTree = new BinaryTreeNodeStub<string, int>(_value2.ToString(), _value2);
+            expectedTree.WithLeftNode(_value3.ToString(), _value3)
+                        .WithRightNode(_value1.ToString(), _value1);
+            var actualTree = new BST<string, int>();
+            actualTree.Insert(_value2.ToString(), _value2);
+            actualTree.Insert(_value3.ToString(), _value3);
+            actualTree.Insert(_value1.ToString(), _value1);
 
-            var tree = new BST<string, int>();
+            //Act
+            actualTree.Reverse();
 
-            tree.Insert(value3.ToString(), value3);
-            tree.Insert(value1.ToString(), value1);
-            tree.Insert(value2.ToString(), value2);
-            CollectionAssert.AreEqual(new List<int> { 1, 2, 3 }, tree.InOrderTreeWalk());
-
-            tree.Delete(value2.ToString());
-            CollectionAssert.AreEqual(new List<int> { 1, 3 }, tree.InOrderTreeWalk());
-        }
-
-        [Test]
-        public void ReverseTo321()
-        {
-            var value1 = 1;
-            var value2 = 2;
-            var value3 = 3;
-
-            var tree = new BST<string, int>();
-
-            tree.Insert(value2.ToString(), value2);
-            tree.Insert(value3.ToString(), value3);
-            tree.Insert(value1.ToString(), value1);
-            CollectionAssert.AreEqual(new List<int> { 1, 2, 3 }, tree.InOrderTreeWalk());
-
-            tree.Reverse();
-            CollectionAssert.AreEqual(new List<int> { 3, 2, 1 }, tree.InOrderTreeWalk());
-        }
-
-        [Test]
-        public void ReverseBigTree()
-        {
-            var values = new List<int> { 2, 1, 3, 4, 5, 6, 7, 9 };
-
-            var tree = new BST<string, int>();
-
-            foreach (var value in values)
-                tree.Insert(value.ToString(), value);
-
-            CollectionAssert.AreEqual(values.OrderBy(x => x), tree.InOrderTreeWalk());
-
-            tree.Reverse();
-            CollectionAssert.AreEqual(values.OrderByDescending(x => x), tree.InOrderTreeWalk());
+            //Assert
+            Assert.AreEqual(expectedTree, actualTree.Root);
         }
     }
 }
