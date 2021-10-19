2bcd98a114a561b7ce0dd955f1b6ae2dfedf40d9
Anton_Zaparivanniy <zaparivannyi@gmail.com>
2021-10-19 10:43:17 +0300

    Небольшой рефакторинг нейминга в RBT Рефакторинг тестов RBT, новый подход тестирования деревьев для RBT

--- CLRS.Tests/Stubs/BinaryTreeNodeStub.cs
+++ CLRS.Tests/Stubs/BinaryTreeNodeStub.cs
@@ -10,6 +10,8 @@ namespace CLRS.Tests.Stubs
     public class BinaryTreeNodeStub<TKey, TValue> : IBinaryTreeNode<TKey, TValue>
         where TKey : class, IComparable<TKey>
     {
+        public static BinaryTreeNodeStub<TKey, TValue> Nil { get; } = new BinaryTreeNodeStub<TKey, TValue>();
+
         private TKey _key;
         private TValue _value;
         private BinaryTreeNodeStub<TKey, TValue> _left;
@@ -33,18 +35,36 @@ namespace CLRS.Tests.Stubs
         {
             this._left = new BinaryTreeNodeStub<TKey, TValue>(key, value);
         }
+        public void SetLeftNode(BinaryTreeNodeStub<TKey, TValue> node)
+        {
+            this._left = node;
+        }
         
         public void SetRightNode(TKey key, TValue value)
         {
             this._right = new BinaryTreeNodeStub<TKey, TValue>(key, value);
         }
+        
+        
+        public void SetRightNode(BinaryTreeNodeStub<TKey, TValue> node)
+        {
+            this._right = node;
+        }
 
+        // TODO: Неудобно дебажить, нет вывода отличий в консоль при падении теста
+        // TODO: Для красно-черных деревьев не сравнивает цвет.
+        // TODO: Также для них не сравнивает факт одного и того же объекта во всех Nil-листьях
         public bool Equals(IBinaryTreeNode<TKey, TValue> compareNode)
         {
+            //TODO: Отрефакторить это место с учетом того, что ключ и значение могут быть пустые в Nil-нодах для RBT
             // Сравниваем текущую ноду
-            if (!_key.Equals(compareNode.Key)) 
+            if (_key == null && compareNode.Key != null 
+                || _key != null && compareNode == null 
+                || (!_key?.Equals(compareNode.Key) ?? false)) 
                 return false;
-            if (!_value.Equals(compareNode.Value)) 
+            if (_value == null && compareNode.Value != null 
+                || _value != null && compareNode.Value == null 
+                || (!_value?.Equals(compareNode.Value) ?? false))
                 return false;
 
             //TODO: Отрефакторить это место с учетом того, что левая или правая поднода могут быть null
