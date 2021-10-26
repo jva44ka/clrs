94c08a6a42a089682f746b7219218918b2e14256
Anton_Zaparivanniy <zaparivannyi@gmail.com>
2021-10-27 02:41:22 +0300

    Рефакторинг тестов на хеш-таблицу Добавлено несколько тестов на добавление в хеш-таблицу

--- CLRS.Tests/Tests/Structures/HashTableTests.cs
+++ CLRS.Tests/Tests/Structures/HashTableTests.cs
@@ -11,24 +11,58 @@ namespace CLRS.Tests.Tests.Structures
     public class HashTableTests
     {
         [Test]
-        public void Add()
+        public void Insert_OneValueToEmptyHashTable_AddedOneValue()
         {
-            var key1 = "Ключик 8495485";
+            //Arrange
+            var key1 = (IntStub) 1;
+            var val1 = "val1";
+            var hashTable = new HashTable<IntStub, string>();
+
+            //Act
+            hashTable.Insert(key1, val1);
+
+            //Assert
+            Assert.AreEqual(1, hashTable.Count);
+            Assert.AreEqual(val1, hashTable[key1]);
+        }
+        
+        [Test]
+        public void Insert_FiveValuesToEmptyHashTable_AddedFiveValues()
+        {
+            //Arrange
+            var numbers = IntStubGenerator.GetFirst100Numbers();
+            var hashTable = new HashTable<IntStub, string>();
+            var expectedValues = IntStubGenerator.GetFirst100Numbers().Take(5).Select(number => number.ToString());
+
+            //Act
+            foreach (var number in numbers.Take(5)) 
+                hashTable.Insert(number, number.ToString());
+
+            //Assert
+            var actualValues = new List<string>();
+            foreach (var number in numbers.Take(5))
+                actualValues.Add((string)hashTable[number]);
+            Assert.AreEqual(5, hashTable.Count);
+            CollectionAssert.AreEqual(expectedValues, actualValues);
+        }
+        
+        [Test]
+        public void Insert_TwoStringValuesToEmptyHashTable_AddedTwoValues()
+        {
+            //Arrange
+            var key1 = "Ключик 8493448";
             var value1 = "Значение №8493448";
             var key2 = "Ключик 8495";
             var value2 = "Значение №8498";
-
             var hashTable = new HashTable<string, string>();
-            Assert.AreEqual(0, hashTable.Count);
-            Assert.AreEqual(null, hashTable[key1]);
-            Assert.AreEqual(null, hashTable[key2]);
 
+            //Act
             hashTable.Insert(key1, value1);
-            Assert.AreEqual(1, hashTable.Count);
-            Assert.AreEqual(value1, hashTable[key1]);
-
             hashTable.Insert(key2, value2);
+
+            //Assert
             Assert.AreEqual(2, hashTable.Count);
+            Assert.AreEqual(value1, hashTable[key1]);
             Assert.AreEqual(value2, hashTable[key2]);
         }
 
